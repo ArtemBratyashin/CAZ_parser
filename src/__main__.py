20a09da8abca3e5a04da35d3f4194635a02902c7
Artem Bratyashin <bratiashin.aa21@physics.msu.ru>
2026-02-14 15:56:32 +0300

    main file created

---
+++ src/__main__.py
@@ -0,0 +1,28 @@
+import datetime as dt
+import logging
+from zoneinfo import ZoneInfo
+
+from config import EnvConfig
+from parsers.tg_parser import TelegramParser
+from text_composer import TextComposer
+from writer_bot import WriterBot
+
+if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO)
+
+    bot = WriterBot(
+        token=EnvConfig().writer_token(),
+        chat_id=EnvConfig().chat_id(),
+        parser=TelegramParser(
+            api_id=EnvConfig().tg_api_id(),
+            api_hash=EnvConfig().tg_api_hash(),
+            phone_number=EnvConfig().phone_number(),
+            session_name="user_session",
+        ),
+        composer=TextComposer(),
+        database='',
+        daily_time=dt.time(15, 56, tzinfo=ZoneInfo("Europe/Moscow")),
+    )
+
+    bot.run()
