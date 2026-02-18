de2d3cdee5549f44d68a3e7942c0b2d8a8d0d41c
Artem Bratyashin <bratiashin.aa21@physics.msu.ru>
2026-02-18 23:21:34 +0300

    config edit

--- src/config.py
+++ src/config.py
@@ -11,47 +11,52 @@ class EnvConfig:
 
     def __init__(self, env_file: str = None) -> None:
         '''Загружаем и проверяем переменные'''
-        load_dotenv(env_file)
+        self._env_file = env_file
 
-        writer_token = os.getenv("WRITER_TOKEN")
-        chat_id = os.getenv("MY_CHAT_ID")
-        tg_api_id = os.getenv("TG_API_ID")
-        tg_api_hash = os.getenv("TG_API_HASH")
-        phone_number = os.getenv("PHONE_NUMBER")
-        db_path = os.getenv("DB_PATH", "departments.sqlite3")
-
-        if not writer_token:
-            raise ValueError("WRITER_TOKEN is missing")
-        if not chat_id:
-            raise ValueError("MY_CHAT_ID is missing")
-        if not tg_api_id:
-            raise ValueError("TG_API_ID is missing")
-        if not tg_api_hash:
-            raise ValueError("TG_API_HASH is missing")
-        if not phone_number:
-            raise ValueError("PHONE_NUMBER is missing")
-
-        self._writer_token = writer_token
-        self._chat_id = int(chat_id)
-        self._db_path = db_path
-        self._tg_api_id = int(tg_api_id)
-        self._tg_api_hash = tg_api_hash
-        self._phone_number = phone_number
 
     def writer_token(self) -> str:
-        return self._writer_token
+        load_dotenv(dotenv_path=self._env_file)
+        writer_token = os.getenv("WRITER_TOKEN")
+        if not writer_token:
+            raise ValueError("WRITER_TOKEN is missing")
+        return writer_token
 
     def chat_id(self) -> int:
-        return self._chat_id
+        load_dotenv(dotenv_path=self._env_file)
+        chat_id = os.getenv("MY_CHAT_ID")
+        if not chat_id:
+            raise ValueError("MY_CHAT_ID is missing")
+        return int(chat_id)
 
     def db_path(self) -> str:
-        return self._db_path
+        load_dotenv(dotenv_path=self._env_file)
+        db_path = os.getenv("DB_PATH", "departments.sqlite3")
+        return db_path
 
     def tg_api_id(self) -> int:
-        return self._tg_api_id
+        load_dotenv(dotenv_path=self._env_file)
+        tg_api_id = os.getenv("TG_API_ID")
+        if not tg_api_id:
+            raise ValueError("TG_API_ID is missing")
+        return tg_api_id
 
     def tg_api_hash(self) -> str:
-        return self._tg_api_hash
+        load_dotenv(dotenv_path=self._env_file)
+        tg_api_hash = os.getenv("TG_API_HASH")
+        if not tg_api_hash:
+            raise ValueError("TG_API_HASH is missing")
+        return tg_api_hash
 
     def phone_number(self) -> str:
-        return self._phone_number
+        load_dotenv(dotenv_path=self._env_file)
+        phone_number = os.getenv("PHONE_NUMBER")
+        if not phone_number:
+            raise ValueError("PHONE_NUMBER is missing")
+        return phone_number
+    
+    def vk_token(self) -> str:
+        load_dotenv(dotenv_path=self._env_file)
+        vk_token = os.getenv("VK_TOKEN")
+        if not vk_token:
+            raise ValueError("VK_TOKEN is missing")
+        return vk_token
